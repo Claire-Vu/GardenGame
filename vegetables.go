f61b1a64c75fb152b546d3ccee0010d0c563e0fd
Elaine Waddle <141589914+elaindle@users.noreply.github.com>
2024-11-11 14:26:57 -0700

    file that maps fruit/veg traits to yen's Fruit and Vegetable structs

---
+++ vegetables.go
@@ -0,0 +1,39 @@
+package main
+
+/*
+	// If implemented, this file is for assigning characteristics to fruit/vegetables.
+
+// data structures created by yen!
+type Vegetable struct {
+	Name           string
+	Cost           int
+	ProductionDays int
+	Product        string
+	SellPrice      int
+	GrowthStage    int // (0 = not planted, 1 = growing, 2 = fully grown)
+}
+
+const parsnip = Vegetable("Parsnip", 20, 4, "Parsnip", 10, 0)
+const potato = Vegetable("Potato", 40, 6, "Potato", 20, 0)
+const garlic = Vegetable("Garlic", 60, 8, "Garlic", 40, 0)
+const corn = Vegetable("Corn", 100, 8, "Corn", 30, 0)
+const pumpkin = Vegetable("Pumpkin", 100, 14, "Pumpkin", 320, 0)
+
+// potential difference: fruits continuing after 1 harvest
+// data structure created by yen!
+type Fruit struct {
+	Name           string
+	Cost           int
+	ProductionDays int
+	Product        string
+	SellPrice      int
+	GrowthStage    int // (0 = not planted, 1 = growing, 2 = fully grown)
+}
+
+const apple = Fruit("Apple", 70, 6, "Apple", 10, 0)
+const orange = Fruit("Orange", 70, 6, "Orange", 10, 0)
+const mango = Fruit("Mango", 110, 8, "Mango", 20, 0)
+const peach = Fruit("Peach", 140, 8, "Peach", 30, 0)
+const banana = Fruit("Banana", 180, 12, "Banana", 45, 0)
+
+*/
