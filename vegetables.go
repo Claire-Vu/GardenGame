1200800a67816b44a74d78d83861399756e02504
Elaine Waddle <141589914+elaindle@users.noreply.github.com>
2024-11-14 20:33:00 -0700

    uncommented vegetables, updated with emojis

--- vegetables.go
+++ vegetables.go
@@ -1,23 +1,23 @@
 package main
 
-/*
-	// If implemented, this file is for assigning characteristics to fruit/vegetables.
+// If implemented, this file is for assigning characteristics to fruit/vegetables.
 
 // data structures created by yen!
 type Vegetable struct {
 	Name           string
 	Cost           int
 	ProductionDays int
-	Product        string
+	TimePlanted    int
+	Symbol         string
 	SellPrice      int
-	GrowthStage    int // (0 = not planted, 1 = growing, 2 = fully grown)
+	fullyGrown     int // (0 = not planted, 1 = growing, 2 = fully grown)
 }
 
-const parsnip = Vegetable("Parsnip", 20, 4, "Parsnip", 10, 0)
-const potato = Vegetable("Potato", 40, 6, "Potato", 20, 0)
-const garlic = Vegetable("Garlic", 60, 8, "Garlic", 40, 0)
-const corn = Vegetable("Corn", 100, 8, "Corn", 30, 0)
-const pumpkin = Vegetable("Pumpkin", 100, 14, "Pumpkin", 320, 0)
+const parsnip = Vegetable("Carrot", 20, 4, 0, "🥕", 10, 0)
+const potato = Vegetable("Potato", 40, 6, 0, "🥔", 20, 0)
+const garlic = Vegetable("Garlic", 60, 8, 0, "🧄", 40, 0)
+const corn = Vegetable("Corn", 100, 8, 0, "🌽", 30, 0)
+const pumpkin = Vegetable("Pumpkin", 100, 14, 0, "🎃", 320, 0)
 
 // potential difference: fruits continuing after 1 harvest
 // data structure created by yen!
@@ -25,15 +25,14 @@ type Fruit struct {
 	Name           string
 	Cost           int
 	ProductionDays int
-	Product        string
+	TimePlanted    int
+	Symbol         string
 	SellPrice      int
 	GrowthStage    int // (0 = not planted, 1 = growing, 2 = fully grown)
 }
 
-const apple = Fruit("Apple", 70, 6, "Apple", 10, 0)
-const orange = Fruit("Orange", 70, 6, "Orange", 10, 0)
-const mango = Fruit("Mango", 110, 8, "Mango", 20, 0)
-const peach = Fruit("Peach", 140, 8, "Peach", 30, 0)
-const banana = Fruit("Banana", 180, 12, "Banana", 45, 0)
-
-*/
+const apple = Fruit("Apple", 70, 6, 0, "🍎", 10, 0)
+const orange = Fruit("Orange", 70, 6, 0, "🍊", 10, 0)
+const mango = Fruit("Mango", 110, 8, 0, "🥭", 20, 0)
+const peach = Fruit("Peach", 140, 8, 0, "🍑", 30, 0)
+const banana = Fruit("Banana", 180, 12, 0, "🍌", 45, 0)
