5b0509c4dddb7bc8b2291fe042fa841646ac2a88
Claire Vu <tnv1@arizona.edu>
2024-11-14 19:53:30 -0700

    Made a basic plot function

--- game.go
+++ game.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+type Crop struct {
+	Name       string
+	Symbol     string
+	fullyGrown bool
+}
 
 // go run .
 func main() {
-	fmt.Println("Hello World!")
+	garden := CreatePlot(5, 5)
+	potato := Crop{Name: "Flower", Symbol: "🥔", fullyGrown: true}
+	mango := Crop{Name: "Mango", Symbol: "🥭", fullyGrown: false}
+	mango2 := Crop{Name: "Mango", Symbol: "🥭", fullyGrown: true}
+	garden.Plant(0, 0, &potato)
+	garden.Plant(2, 2, &mango)
+	garden.Plant(2, 3, &mango2)
+	garden.printGarden()
 }
