84438eb420c037d7ee9fce4f3b68d97b015e19f8
Elaine Waddle <141589914+elaindle@users.noreply.github.com>
2024-11-17 12:21:44 -0700

    implemented cropDict

--- vegetables.go
+++ vegetables.go
@@ -14,6 +14,30 @@ type Crop struct {
 	FullyGrown     bool // (0 = not planted, 1 = growing, 2 = fully grown)
 }
 
+//TO DO: createCrop(cropName) returns the crop object created
+//by the crop name passed in.
+//Allows for much neater crop creation in main
+
+cropDict := map[string]Crop{
+
+	//Vegetables: object deleted after harvest
+	"carrot": Crop{"Vegetable", "Carrot", 20, 4, 0, "🥕", 10, false},
+	"potato": Crop{"Vegetable", "Potato", 40, 6, 0, "🥔", 20, false},
+	"garlic": Crop{"Vegetable", "Garlic", 60, 8, 0, "🧄", 40, false},
+	"corn": Crop{"Vegetable", "Corn", 100, 8, 0, "🌽", 30, false},
+	"pumpkin": Crop{"Vegetable", "Pumpkin", 100, 14, 0, "🎃", 320, false},
+	//Fruits: TimePlanted reset to zero after harvest
+	"apple": Crop{"Fruit", "Apple", 70, 6, 0, "🍎", 10, false},
+	"orange": Crop{"Fruit", "Orange", 70, 6, 0, "🍊", 10, false},
+	"mango": Crop{"Fruit", "Mango", 110, 8, 0, "🥭", 20, false},
+	"peach": Crop{"Fruit", "Peach", 140, 8, 0, "🍑", 30, false},
+	"banana": Crop{"Fruit", "Banana", 180, 12, 0, "🍌", 45, false},
+},
+
+func createCrop(cropName string) {
+	return cropDict[cropName]
+}
+
 //Vegetables: object deleted after harvest
 var carrot = Crop{"Vegetable", "Carrot", 20, 4, 0, "🥕", 10, false}
 var potato = Crop{"Vegetable", "Potato", 40, 6, 0, "🥔", 20, false}
